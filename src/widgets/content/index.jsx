43694adafc92066e33ea9e496a3cfb8985df5d5f
Asipko <avsipko@gmail.com>
2024-11-07 11:00:28 +0300

    add

--- src/widgets/content/index.jsx
+++ src/widgets/content/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import './style.css'
 
 const Content = observer(({ type }) => {
     const navigate = useNavigate();
+
+    useEffect(() => {
+        window.addEventListener("load",function() {
+            setTimeout(function(){
+                // This hides the address bar:
+                window.scrollTo(0, 1);
+            }, 0);
+        });
+    }, [])
 
     return (
         <div className='margin-1-04-center padding-05 width-height'>
