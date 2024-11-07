9ee6d83f124cb2992299ab6774dcd9334a6a6d23
Asipko <avsipko@gmail.com>
2024-11-07 11:13:36 +0300

    add

--- src/widgets/content/index.jsx
+++ src/widgets/content/index.jsx
@@ -6,14 +6,6 @@ import './style.css'
 const Content = observer(({ type }) => {
     const navigate = useNavigate();
 
-    useEffect(() => {
-        window.addEventListener("load",function() {
-            setTimeout(function(){
-                // This hides the address bar:
-                window.scrollTo(0, 1);
-            }, 0);
-        });
-    }, [])
 
     return (
         <div className='margin-1-04-center padding-05 width-height'>
